9d38cd9190a3ad7660441f5546a5230fd2104c61
ElijahCoding <cuiyeqing960904@gmail.com>
2019-11-30 22:14:08 +0300

    passing variables

--- Tutorial/Tutorial-1.py
+++ Tutorial/Tutorial-1.py
@@ -2,9 +2,13 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/home")
+@app.route("/")
 def home():
     return 'Hello, <h1>World</h1>'
+
+@app.route("/<name>")
+def user(name):
+    return f"Hello {name}"
 
 if __name__ == "__main__":
     app.run()
