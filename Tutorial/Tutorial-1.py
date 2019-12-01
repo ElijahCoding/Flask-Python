3bc02c20fe162f36106d1334052288e29bd1a0d2
ElijahCoding <cuiyeqing960904@gmail.com>
2019-12-01 14:59:53 +0300

    basic http methods

--- Tutorial/Tutorial-1.py
+++ Tutorial/Tutorial-1.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, url_for, render_template
+from flask import Flask, redirect, url_for, render_template, request
 
 app = Flask(__name__)
 
@@ -8,11 +8,15 @@ def home():
 
 @app.route('/login', methods=['POST', 'GET'])
 def login():
-    return render_template('login.html')
+    if request.method == 'POST':
+        user = request.form['nm']
+        return redirect(url_for('user', usr=user))
+    else:
+        return render_template('login.html')
 
 @app.route('/<usr>', methods=['POST', 'GET'])
 def user(usr):
-    return "<h1>{usr}</h1>"
+    return f"<h1>{usr}</h1>"
 
 
 if __name__ == "__main__":
