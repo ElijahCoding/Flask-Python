e4061a5e7ef1137d96c41f5c582d03523ae7852e
ElijahCoding <cuiyeqing960904@gmail.com>
2019-12-02 15:34:42 +0300

    connected to db

--- FirstSite/app.py
+++ FirstSite/app.py
@@ -1,10 +1,21 @@
 import flask
+import os
+import data.db_session as db_session
 
 app = flask.Flask(__name__)
 
 def main():
     register_blueprints()
+    setup_db()
     app.run(debug=True)
+
+def setup_db():
+    db_file = os.path.join(
+        os.path.dirname(__file__),
+        'db',
+        'data.sqlite')
+
+    db_session.global_init(db_file)
 
 def register_blueprints():
     from views import home_views, package_views, account_views, cms_views
