059a4b9c764fb8bf8987401addbb4f1201fe4b4d
ElijahCoding <cuiyeqing960904@gmail.com>
2019-12-03 10:40:15 +0300

    db connection

--- RedditClone/app.py
+++ RedditClone/app.py
@@ -1,6 +1,9 @@
 from flask import *
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:root@localhost:8889/fresh'
+db = SQLAlchemy(app)
 
 @app.route('/')
 def index():
