fa6e15cf91da519e542964c28a5c6e1651ac624d
ElijahCoding <cuiyeqing960904@gmail.com>
2019-12-03 09:43:22 +0300

    delete

--- TodoList/app.py
+++ TodoList/app.py
@@ -30,6 +30,16 @@ def index():
         tasks = Todo.query.order_by(Todo.date_created).all()
         return render_template('index.html', tasks=tasks)
 
+@app.route('/delete/<int:id>')
+def delete(id):
+    task_to_delete = Todo.query.get_or_404(id)
+
+    try:
+        db.session.delete(task_to_delete)
+        db.session.commit()
+        return redirect('/')
+    except:
+        return 'Error deleting'
 if __name__ == '__main__':
     db.create_all()
     app.run(debug=True)
