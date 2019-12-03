0000e7152dc09b385f6b96fa97a919431d7226cf
ElijahCoding <cuiyeqing960904@gmail.com>
2019-12-03 09:09:50 +0300

    create task through db session

--- TodoList/app.py
+++ TodoList/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
@@ -16,7 +16,19 @@ class Todo(db.Model):
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    if request.method == 'POST':
+        task_content = request.form['content']
+        new_task = Todo(content=task_content)
+
+        try:
+            db.session.add(new_task)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'There was an issue'
+    else:
+        tasks = Todo.query.order_by(Todo.date_created).all()
+        return render_template('index.html', tasks=tasks)
 
 if __name__ == '__main__':
     db.create_all()
