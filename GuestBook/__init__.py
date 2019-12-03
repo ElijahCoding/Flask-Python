624780ee6b802ecea8e876f11b32ef6dfff408b2
ElijahCoding <cuiyeqing960904@gmail.com>
2019-12-03 12:54:43 +0300

    connected to db , building models

--- GuestBook/__init__.py
+++ GuestBook/__init__.py
@@ -1,11 +1,15 @@
 # export FLASK_APP=GuestBook
 from flask import *
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
 
-    from .views import main
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:root@127.0.0.1:8889/fresh'
 
+    from .views import main
     app.register_blueprint(main)
 
     return app
--- GuestBook/views.py
+++ GuestBook/views.py
@@ -1,7 +1,18 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 
 main = Blueprint('main', __name__)
 
 @main.route('/')
 def index():
     return render_template('index.html')
+
+@main.route('/sign')
+def sign():
+    return render_template('sign.html')
+
+@main.route('/sign', methods=['POST'])
+def sign_post():
+    name = request.form.get('name')
+    comment = request.form.get('comment')
+
+    return f'Name: {name}, Comment: {comment}'
