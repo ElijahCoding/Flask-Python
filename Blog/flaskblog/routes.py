db83c81ed7039c00b1d93b7023eec80119c812f0
ElijahCoding <cuiyeqing960904@gmail.com>
2019-12-04 20:13:13 +0300

    delete post

--- Blog/flaskblog/routes.py
+++ Blog/flaskblog/routes.py
@@ -142,4 +142,10 @@ def update_post(post_id):
 @app.route('/post/<int:post_id>/delete', methods=['POST'])
 @login_required
 def delete_post(post_id):
-    pass
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    db.session.delete(post)
+    db.session.commit()
+    flash('Your post has been deleted!', 'success')
+    return redirect(url_for('home'))
