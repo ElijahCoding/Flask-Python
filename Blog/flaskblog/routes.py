abf2b90ea11d9aff1604bf2cef06be29186aee27
ElijahCoding <cuiyeqing960904@gmail.com>
2019-12-04 20:10:18 +0300

    update post

--- Blog/flaskblog/routes.py
+++ Blog/flaskblog/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, url_for, flash, redirect, request
+from flask import render_template, url_for, flash, redirect, request, abort
 from flaskblog import app, db, bcrypt
 from flaskblog.forms import RegistrationForm, LoginForm, UpdateAccountForm, PostForm
 from flaskblog.models import User, Post
@@ -119,10 +119,25 @@ def post(post_id):
     post = Post.query.get_or_404(post_id)
     return render_template('post.html', title=post.title, post=post)
 
-@app.route('/post/<int:post_id>/update')
+@app.route('/post/<int:post_id>/update', methods=['POST', 'GET'])
 @login_required
 def update_post(post_id):
-    pass
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    form = PostForm()
+    if form.validate_on_submit():
+        post.title = form.title.data
+        post.content = form.content.data
+        db.session.commit()
+        flash('Your post has been updated!', 'success')
+        return redirect(url_for('post', post_id=post.id))
+    elif request.method == 'GET':
+        form.title.data = post.title
+        form.content.data = post.content
+
+    return render_template('create_post.html', title='Update Post',
+                           form=form, legend='Update Post')
 
 @app.route('/post/<int:post_id>/delete', methods=['POST'])
 @login_required
